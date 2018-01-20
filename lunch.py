6b13b1ae029dce08a519423009f9f0d24748ab80
Przemek_Pobrotyn <przemek@bothouse.co>
2018-01-20 20:15:25 +0100

    Added functions for validating output and fetching lunches for all defined restaurants.

--- lunch.py
+++ lunch.py
@@ -4,70 +4,81 @@ import facebook
 import sys
 import datetime
 
-# just playing around
-graph = facebook.GraphAPI(access_token=USER_TOKEN)
-# resp = graph.request('/search?q=Poetry&type=event&limit=10000')
 
-# fetch posts
-resp = graph.get_object(
-    id="centrumswiatacom",
-    fields='posts.limit({})'.format(posts_limit))
-# iterate over posts to find the most recent containing one of the keywords
-message = None
-for post in resp['posts']['data']:
-    try:
-        if any(word in post['message'] for word in keywords):
-            message = post['message']
-            print(post['created_time'])
-            print(post['created_time'][:10])
-            print(message)
-            print('~~~~~~~~~~~~~~~~~~~')
-    except KeyError:
-        pass
-
-if not message:
-    print('No lunch posts yet, redo.')
-    sys.exit(1)
-
-
-def date_today():
+def _date_today():
     now = datetime.datetime.now()
     return now.strftime("%Y-%m-%d")
 
 
-def start_graph(access_token = USER_TOKEN):
+def start_graph(access_token=USER_TOKEN):
     graph = facebook.GraphAPI(access_token=access_token)
     return graph
 
 
-def fetch_restaurants_posts(graph, restaurant_name, limit=posts_limit):
+def _fetch_restaurant_posts(graph, restaurant_id, limit=posts_limit):
     resp = graph.get_object(
-        id=restaurant_name,
+        id=restaurant_id,
         fields='posts.limit({})'.format(limit)
     )
     return resp
 
 
-def is_about_lunch(post, keywords):
-    return any(word in post['message'] for word in keywords)
+def _is_about_lunch(post, keywords):
+    return any(word in post['message'].split() for word in keywords)
 
 
-def is_today(post):
-    return post['created_time'][:10] == date_today()
+def _is_today(post):
+    print(post['created_time'][:10])
+    print(_date_today())
+    return post['created_time'][:10] == _date_today()
 
 
-def find_todays_lunch(resp, keywords=keywords):
+def _find_todays_lunch_single_restaurant(resp, keywords=keywords):
     message = None
     for post in resp['posts']['data']:
         try:
-            if is_about_lunch(post, keywords) and is_today(post):
+            if _is_about_lunch(post, keywords) and _is_today(post):
                 message = post['message']
         except KeyError:
             pass
     return message
 
-def send_menu(message, mailing_list):
+
+def find_todays_lunch_all_restaurants(
+        graph, restaurants=restaurants, keywords=keywords, limit=posts_limit):
+
+    lunches = {}
+    for rest_name, rest_id in restaurants.items():
+        resp = _fetch_restaurant_posts(graph, rest_id, limit)
+        lunches[rest_name] = _find_todays_lunch_single_restaurant(
+            resp, keywords)
+    print(lunches)
+    return lunches
 
 
-def was_lunch_found(message):
-    if message:
+def _check_lunches(lunches_dict):
+    if not all(lunches_dict.values()):
+        return False
+    else:
+        return True
+
+
+def send_menu(lunches_dict, mailing_list):
+    if _check_lunches(lunches_dict):
+        # send out mailing to mailing_list
+        return True
+    else:
+        return False
+
+
+def exit_script(bool):
+    if not bool:
+        print("One of the restaurants has not posted about today's lunch yet")
+        print("Try again later.")
+        sys.exit(1)
+
+if __name__ == '__main__':
+    graph = start_graph()
+    lunches = find_todays_lunch_all_restaurants(graph)
+    b = send_menu(lunches, mailing_list)
+    exit_script(b)
