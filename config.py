01479eff21ea35786d9624ace0b42c0372b8abb0
Przemek_Pobrotyn <przemek@bothouse.co>
2018-01-20 21:24:52 +0100

    Extended the keywords list

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 restaurants = {'Centrum': 'centrumswiatacom',
                'Boska': 'boskapraga'}
 posts_limit = 5
-keywords = ('lunch', 'obiad', 'lunchu', 'obiadek', 'lunchyk')
-mailing_list = ('',)
+keywords = ('lunch', 'obiad', 'lunchu', 'obiadek', 'lunchyk', 'LUNCH',
+            'OBIAD', 'LUNCHYK', '#LUNCH', '#lunch', '#obiad', '#obiadek',
+            '#lunchyk')
