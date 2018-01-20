c2f47f7fef2aa5edf872274cb8c420e3c326fd25
Przemek_Pobrotyn <przemek@bothouse.co>
2018-01-20 22:14:04 +0100

    Added Klub Bazar to list of restaurants.

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 restaurants = {'Centrum': 'centrumswiatacom',
-               'Boska': 'boskapraga'}
+               'Boska': 'boskapraga',
+               'Bazar Klub': 'BazarKlub'}
 posts_limit = 5
 keywords = ('lunch', 'obiad', 'lunchu', 'obiadek', 'lunchyk', 'LUNCH',
             'OBIAD', 'LUNCHYK', '#LUNCH', '#lunch', '#obiad', '#obiadek',
