d11f65da738713626f6d9db81ddbf58059d062fd
Przemek_Pobrotyn <przemek@bothouse.co>
2018-01-24 15:19:27 +0100

    Added printing a date to improve logs

--- lunch.sh
+++ lunch.sh
@@ -2,5 +2,5 @@
 cd /Users/Przemek/Desktop/lunch_script
 source lunch_script/bin/activate
 date
-RET=1;until [[ $RET = 0 ]];do python lunch.py; RET="$?";sleep 900;done
+RET=1;until [[ $RET = 0 ]];do python lunch.py; RET="$?";sleep 900; date;done
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
