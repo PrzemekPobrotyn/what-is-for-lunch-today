d01f6ebad639a62cb50c77f4b414c0d9b16351b8
Przemek_Pobrotyn <przemek@bothouse.co>
2018-01-25 15:14:12 +0100

    Changed keywords list and raised posts limit to ensure correct capture on Fridays even on busy weeks

--- config/config.py
+++ config/config.py
@@ -1,11 +1,9 @@
 restaurants = {'Centrum': 'centrumswiatacom',
-               'Boska': 'boskapraga',
-               'Bazar Klub': 'BazarKlub'}
+               'Boska': 'boskapraga'}
+               # 'Bazar Klub': 'BazarKlub'}
 weekly_menus = ('Centrum', )
-posts_limit = 15
-keywords = ('lunch', 'obiad', 'lunchu', 'obiadek', 'lunchyk', 'LUNCH',
-            'OBIAD', 'LUNCHYK', '#LUNCH', '#lunch', '#obiad', '#obiadek',
-            '#lunchyk', 'LUNCH]')
+posts_limit = 20
+keywords = ('lunch', 'obiad', 'LUNCH', 'OBIAD', '#LUNCH', '#lunch', '#obiad')
 
 days_list = ['PONIEDZIAŁEK', 'WTOREK', 'ŚRODA', 'CZWARTEK', 'PIĄTEK', 'SOBOTA',
              'NIEDZIELA']
