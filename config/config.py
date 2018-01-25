b6fd95cfad0045f66af1ea61558f27fd346e2896
Przemek_Pobrotyn <przemek@bothouse.co>
2018-01-25 01:09:51 +0100

    Extended the posts limit to 15 to deal with Centrum pinned posts from Monday of each week

--- config/config.py
+++ config/config.py
@@ -2,7 +2,7 @@ restaurants = {'Centrum': 'centrumswiatacom',
                'Boska': 'boskapraga',
                'Bazar Klub': 'BazarKlub'}
 weekly_menus = ('Centrum', )
-posts_limit = 5
+posts_limit = 15
 keywords = ('lunch', 'obiad', 'lunchu', 'obiadek', 'lunchyk', 'LUNCH',
             'OBIAD', 'LUNCHYK', '#LUNCH', '#lunch', '#obiad', '#obiadek',
             '#lunchyk', 'LUNCH]')
