1ec4f06e10e8241fa796aa95ddc5ddfa8e9d4bb6
Przemek_Pobrotyn <przemek@bothouse.co>
2018-01-27 13:02:04 +0100

    Added parsing command line arguments

--- phone_call.py
+++ phone_call.py
@@ -1,24 +1,31 @@
-# NOTES:
-
-# Nexmo seems complicated
-# Check if twilio is capable of synthesisng Polish
-# purchase a phone number,
-# it seems like you'll have to spin up a simple flask app for twilio, which will
-# return a twilo xml with instructions on what to do
-
-
+import argparse
 from twilio.rest import Client
+from config.credentials import (twilio_account_sid,
+                                twilio_auth_token,
+                                twilio_phone_number)
 
-# Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "ACeef64fa61ad2133ed1966de9b50c30bd"
-auth_token = "001adba6a3434660c880e2770ae7de85"
+from config.config import yellow_pages
 
-client = Client(account_sid, auth_token)
 
-call = client.calls.create(
-    to="+48725741259",
-    from_="+48717375227",
-    url="http://0767990c.ngrok.io/xml/21"
-)
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Make a phone call and order lunches.')
 
-# sth along these lines, turn it into a function etc
+    parser.add_argument(
+        'url',
+        help='URL of active ngrok connection with number of lunches specified')
+
+    parser.add_argument(
+        'to',
+        help='Phone number to be called - restaurant\'s phone number.')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    client = Client(twilio_account_sid, twilio_auth_token)
+    call = client.calls.create(
+        to=yellow_pages[args.to],
+        from_=twilio_phone_number,
+        url=args.url)
