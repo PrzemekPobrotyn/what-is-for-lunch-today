7939ef79c1c01bc767df34f37a368e76701170c1
Przemek_Pobrotyn <przemek@bothouse.co>
2018-01-27 13:02:23 +0100

    Added yellow_pages (a phone book) dictonary

--- config/config.py
+++ config/config.py
@@ -7,3 +7,7 @@ keywords = ('lunch', 'obiad', 'LUNCH', 'OBIAD', '#LUNCH', '#lunch', '#obiad')
 
 days_list = ['PONIEDZIAŁEK', 'WTOREK', 'ŚRODA', 'CZWARTEK', 'PIĄTEK', 'SOBOTA',
              'NIEDZIELA']
+
+yellow_pages = {'Centrum': '226182197', # do I need to add 048 or 48 in fron of the number?
+                'Boska': '+48605781807',
+                'test_phone': '+48725741259'}
