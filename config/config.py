f1bbd19327d3532b56040fdf740bb33ecf17238d
Przemek_Pobrotyn <przemek@bothouse.co>
2018-01-27 19:48:44 +0100

    Cosmetic changes to config

--- config/config.py
+++ config/config.py
@@ -1,6 +1,5 @@
-restaurants = {'Centrum': 'centrumswiatacom',
-               'Boska': 'boskapraga'}
-               # 'Bazar Klub': 'BazarKlub'}
+restaurants = {'Centrum': 'centrumswiatacom', 'Boska': 'boskapraga'}
+
 weekly_menus = ('Centrum', )
 posts_limit = 20
 keywords = ('lunch', 'obiad', 'LUNCH', 'OBIAD', '#LUNCH', '#lunch', '#obiad')
@@ -8,6 +7,6 @@ keywords = ('lunch', 'obiad', 'LUNCH', 'OBIAD', '#LUNCH', '#lunch', '#obiad')
 days_list = ['PONIEDZIAŁEK', 'WTOREK', 'ŚRODA', 'CZWARTEK', 'PIĄTEK', 'SOBOTA',
              'NIEDZIELA']
 
-yellow_pages = {'Centrum': '226182197', # do I need to add 048 or 48 in fron of the number?
+yellow_pages = {'Centrum': '226182197', # do I need to add 048 or 48 in front of the number?
                 'Boska': '+48605781807',
                 'test_phone': '+48725741259'}
