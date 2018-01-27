4829ecef4f7df40162723c3c86b0687058206843
Przemek_Pobrotyn <przemek@bothouse.co>
2018-01-27 14:29:39 +0100

    Modified keywords list

--- config/config.py
+++ config/config.py
@@ -1,8 +1,7 @@
 restaurants = {'Centrum': 'centrumswiatacom', 'Boska': 'boskapraga'}
-               # 'Bazar Klub': 'BazarKlub'}
 weekly_menus = ('Centrum', )
 posts_limit = 20
-keywords = ('lunch', 'obiad', 'LUNCH', 'OBIAD', '#LUNCH', '#lunch', '#obiad')
+keywords = ('lunch', 'obiad', 'LUNCH', 'OBIAD')
 
 days_list = ['PONIEDZIAŁEK', 'WTOREK', 'ŚRODA', 'CZWARTEK', 'PIĄTEK', 'SOBOTA',
              'NIEDZIELA']
