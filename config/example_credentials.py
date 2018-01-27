517f5a41bd07fa20098dd03f85a305cefa6aee74
Przemek_Pobrotyn <przemek@bothouse.co>
2018-01-27 14:33:48 +0100

    Added example credentials and mailing list files to be published on github

---
+++ config/example_credentials.py
@@ -0,0 +1,7 @@
+# your credentials.py should look as follows
+
+USER_TOKEN = 'your facebook user token'
+email_address = 'email address to send out menus'
+email_password = 'email password'
+admin_email = 'admin email to sent error messages'
+slack_webhook = 'slack webhook to post to slack'
---
+++ config/example_mailing_list.py
@@ -0,0 +1,3 @@
+# your mailing_list.py should contain a tuple of e-mail addresses, as below
+
+mailing_list = ('example@example.com', 'second_example@example.com')
